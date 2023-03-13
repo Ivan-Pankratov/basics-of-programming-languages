6d716f483192d8bd52051e667905477b5753acd6
Иван <pankratovivan1978@yandex.ru>
2023-03-13 23:37:50 +0300

    Задача 3, вывод дня недели по номеру

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -9,35 +9,35 @@
 Console.Write("Введите номер дня недели ");
 string day = Console.ReadLine();
 Console.Write("Вашему номеру дня недели соответствует ");
-if (day == "1") 
-{ 
+if (day == "1")
+{
     Console.WriteLine("понедельник");
 }
 else if (day == "2")
-    {
-        Console.WriteLine("вторник");
-    }
-    else if (day == "3")
-        {
-            Console.WriteLine("среда");
-        }
-        else if (day == "4")
-            {
-                Console.WriteLine("четверг");
-            }
-             else if (day == "5")
-                {
-                    Console.WriteLine("пятница");
-                }
-                else if (day == "6")
-                    {
-                        Console.WriteLine("суббота");
-                    }
-                    else if (day == "7")
-                        {
-                            Console.WriteLine("воскресенье");
-                        }
-                         else 
-                        {
-                            Console.WriteLine("ошибка. Простите, в нашем календаре нет такого дня недели.");
-                        }
+{
+    Console.WriteLine("вторник");
+}
+else if (day == "3")
+{
+    Console.WriteLine("среда");
+}
+else if (day == "4")
+{
+    Console.WriteLine("четверг");
+}
+else if (day == "5")
+{
+    Console.WriteLine("пятница");
+}
+else if (day == "6")
+{
+    Console.WriteLine("суббота");
+}
+else if (day == "7")
+{
+    Console.WriteLine("воскресенье");
+}
+else
+{
+    Console.WriteLine("ошибка. Простите, в нашем календаре нет такого дня недели.");
+}
