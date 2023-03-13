52678f4c04f323d5c42e47b681aff119c02512d8
Иван <pankratovivan1978@yandex.ru>
2023-03-13 23:56:20 +0300

    Задача 5, вывод целых чисел от -N до N

---
+++ Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход одно целое число (N),
+// а на выходе даёт все целые числа на промежутке от - N до N
+// 2 -> -2. -1. 0. 1. 2
+// Алгоритм:
+// ввод числа
+//
+//
+
+Console.Write("Введите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count}, ");
+    count ++;
+}
