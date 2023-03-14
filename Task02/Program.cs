f969866220c7c1ff48939f688ab7f37afa46bf3a
Иван <pankratovivan1978@yandex.ru>
2023-03-14 23:16:53 +0300

    Готовы решения 2 и 4 задач

---
+++ Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Алгоритм:
+// 1. ВВод переменных a и b;
+// 2. Если a > b, печатаем  max = a, min = b
+//    иначе печатаем  max = b, min = a
+//
+Console.Write("Введите целое число, a =  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число, b =  ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) 
+{   
+    Console.WriteLine($"max = {a}, min = {b}");
+}
+else 
+{
+    Console.WriteLine($"max = {b}, min = {a}");
+}
---
+++ Task04/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+//
+// Алгоритм:
+// 1. Ввод целых чисел a, b, c;
+// 2. Определение max Если a < b, то, если a<c max = a,
+//                      иначе max = c
+// 3. Вывод max.
+//
+
+Console.Write("Введите целое число, a =  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число, b =  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число, c =  ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{ 
+    if (a>c)
+    {
+        Console.Write($"Максимум = {a}");
+    }
+    else 
+    {
+        Console.Write($"Максимум = {c}");
+    }
+}
+else
+{
+    if (b>c)
+    {
+        Console.Write($"Максимум = {b}");
+    }
+    else 
+    {
+        Console.Write($"Максимум = {c}");
+    }
+}
+
+
+
+
