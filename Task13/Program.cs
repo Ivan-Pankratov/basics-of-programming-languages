9ba2f5307d2070e6c192d08a010e5d28b1b86679
Иван <pankratovivan1978@yandex.ru>
2023-03-17 22:17:42 +0300

    Задание по 2 семинару: задачи 10 и 13 сделаны попросту, задача 15 через Метод

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+//
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите целое число a:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a /= 100) == 0)
+{
+    Console.Write("В вашем числе нет третьей цифры");
+}
+else
+{
+    int b = a;
+    int discharge = 1;
+    while ((b /= 100) != 0)
+    {
+        b = b /= 10;
+        discharge = discharge *10;
+    }
+    a = a /= discharge;
+    int figure = a % 10;
+    Console.Write($"Третья цифра {figure}");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+//
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите цифру дня недели  ");
+string day = Console.ReadLine();
+Weekend(day);
+
+void Weekend(string day)
+{
+    if (day == "6")
+    {
+        Console.WriteLine("Выходной");
+    }
+
+    else
+    { 
+        if (day == "7")
+    {
+        Console.WriteLine("Выходной");
+    }
+       else {Console.WriteLine("Невыходной");}
+    }
+}
