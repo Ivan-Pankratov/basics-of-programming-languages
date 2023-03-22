99eac4b2101ee36d83a6153916f641f6d41dbbe0
Иван <pankratovivan1978@yandex.ru>
2023-03-22 16:31:53 +0300

    Отработаны задачи семинара

---
+++ Nask 35/Program.cs
@@ -0,0 +1,35 @@
+﻿// 
+// Массив с вещественными элементами
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        else
+        {
+            Console.WriteLine(arr[i] + " ]");
+        }
+    }
+}
+
+double[] array = CreateArrayRndDouble(5, 1, 25);
+PrintArray(array);
